cbe260fdfe7666f26ec2b9600918cd64adc3b39e
spolya <32613978+spolya@users.noreply.github.com>
2020-03-21 19:39:18 +0700

    projectTest

---
+++ Source/SchoolFPS/SchoolFPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SchoolFPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCHOOLFPS_API ASchoolFPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
